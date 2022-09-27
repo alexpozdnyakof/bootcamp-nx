3b63458320050f2953f678133af5515d3d1fec42
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-09-27 17:58:21 +0700

    setting up linter and pre-commit

--- apps/bootcamp/src/app/app.spec.tsx
+++ apps/bootcamp/src/app/app.spec.tsx
@@ -1,21 +1,21 @@
-import { cleanup, getByText, render, waitFor } from '@testing-library/react';
-import React from 'react';
-import App from './app';
+import { cleanup, getByText, render, waitFor } from '@testing-library/react'
+import React from 'react'
+import App from './app'
 
 describe('App', () => {
-  afterEach(() => {
-    delete global['fetch'];
-    cleanup();
-  });
+	afterEach(() => {
+    delete global['fetch']
+		cleanup()
+	})
 
-  it('should render successfully', async () => {
-    global['fetch'] = jest.fn().mockResolvedValueOnce({
-      json: () => ({
-        message: 'my message',
-      }),
-    });
+	it('should render successfully', async () => {
+		global['fetch'] = jest.fn().mockResolvedValueOnce({
+			json: () => ({
+				message: 'my message',
+			}),
+		})
 
-    const { baseElement } = render(<App />);
-    await waitFor(() => getByText(baseElement, 'my message'));
-  });
-});
+		const { baseElement } = render(<App />)
+		await waitFor(() => getByText(baseElement, 'my message'))
+	})
+})
--- apps/bootcamp/src/app/app.tsx
+++ apps/bootcamp/src/app/app.tsx
@@ -1,28 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import { Message } from '@bootcamp-nx/api-interfaces';
+import { Message } from '@bootcamp-nx/api-interfaces'
+import { useEffect, useState } from 'react'
 
-export const App = () => {
-  const [m, setMessage] = useState<Message>({ message: '' });
+export function App() {
+	const [m, setMessage] = useState<Message>({ message: '' })
 
-  useEffect(() => {
-    fetch('/api')
-      .then((r) => r.json())
-      .then(setMessage);
-  }, []);
+	useEffect(() => {
+		fetch('/api')
+			.then(r => r.json())
+			.then(setMessage)
+	}, [])
 
-  return (
-    <>
-      <div style={{ textAlign: 'center' }}>
-        <h1>Welcome to bootcamp!</h1>
-        <img
-          width="450"
-          src="https://raw.githubusercontent.com/nrwl/nx/master/images/nx-logo.png"
-          alt="Nx - Smart, Fast and Extensible Build System"
-        />
-      </div>
-      <div>{m.message}</div>
-    </>
-  );
-};
+	return (
+		<>
+			<div style={{ textAlign: 'center' }}>
+				<h1>Welcome to bootcamp!</h1>
+				<img
+					width='450'
+					src='https://raw.githubusercontent.com/nrwl/nx/master/images/nx-logo.png'
+					alt='Nx - Smart, Fast and Extensible Build System'
+				/>
+			</div>
+			<div>{m.message}</div>
+		</>
+	)
+}
 
-export default App;
+export default App
