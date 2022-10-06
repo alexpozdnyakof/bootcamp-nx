9d560b1707a44049fb9e8b9524ea6fb306ae59f3
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-10-06 20:11:54 +0700

    feat(core-ui): added basic box component for space options incapsulation

---
+++ libs/core-ui/src/lib/common-types.ts
@@ -0,0 +1,10 @@
+export type Space =
+	| 'xsmall'
+	| 'small'
+	| 'medium'
+	| 'large'
+	| 'xlarge'
+	| 'xxlarge'
+export type SpaceWithNegatives = Space | `-${Space}`
+
+export type Tone = 'normal' | 'secondary' | 'danger'
