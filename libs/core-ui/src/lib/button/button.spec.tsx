bbb5b3fef0c87107d32723728e33a0f9c5d9c1f8
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-10-08 03:07:21 +0700

    feat(core-ui): add button component

--- libs/core-ui/src/lib/button/button.spec.tsx
+++ libs/core-ui/src/lib/button/button.spec.tsx
@@ -1,10 +1,196 @@
-import { render } from '@testing-library/react'
-
+import { screen, render, fireEvent } from '@testing-library/react'
 import Button from './button'
 
 describe('Button', () => {
-	it('should render successfully', () => {
-		const { baseElement } = render(<Button>Button</Button>)
-		expect(baseElement).toBeTruthy()
+	it('renders a semantic button', () => {
+		render(<Button variant='primary'>Click me</Button>)
+		expect(
+			screen.getByRole('button', { name: 'Click me' })
+		).toBeInTheDocument()
+	})
+
+	it('should apply different class names based on variant', () => {
+		const { rerender } = render(
+			<Button data-testid='button'>Button</Button>
+		)
+		const button = screen.getByTestId('button')
+
+		expect(button).toHaveClass(
+			'variant-primary',
+			'tone-normal',
+			'size-normal'
+		)
+		expect(button).not.toHaveClass('variant-secondary')
+		expect(button).not.toHaveClass('variant-tertiary')
+		expect(button).not.toHaveClass('variant-quaternary')
+
+		rerender(<Button variant='secondary'>Click me</Button>)
+		expect(button).toHaveClass(
+			'variant-secondary',
+			'tone-normal',
+			'size-normal'
+		)
+		expect(button).not.toHaveClass('variant-primary')
+		expect(button).not.toHaveClass('variant-tertiary')
+		expect(button).not.toHaveClass('variant-quaternary')
+
+		rerender(<Button variant='tertiary'>Click me</Button>)
+		expect(button).toHaveClass(
+			'variant-tertiary',
+			'tone-normal',
+			'size-normal'
+		)
+		expect(button).not.toHaveClass('variant-primary')
+		expect(button).not.toHaveClass('variant-secondary')
+		expect(button).not.toHaveClass('variant-quaternary')
+
+		rerender(<Button variant='quaternary'>Click me</Button>)
+		expect(button).toHaveClass(
+			'variant-quaternary',
+			'tone-normal',
+			'size-normal'
+		)
+		expect(button).not.toHaveClass('variant-primary')
+		expect(button).not.toHaveClass('variant-secondary')
+		expect(button).not.toHaveClass('variant-tertiary')
+	})
+
+	it('applies different class names based on tone', () => {
+		const { rerender } = render(<Button variant='primary'>Click me</Button>)
+		const button = screen.getByRole('button', { name: 'Click me' })
+		expect(button).toHaveClass(
+			'variant-primary',
+			'tone-normal',
+			'size-normal'
+		)
+		expect(button).not.toHaveClass('tone-destructive')
+
+		rerender(
+			<Button variant='primary' tone='destructive'>
+				Click me
+			</Button>
+		)
+		expect(button).toHaveClass(
+			'variant-primary',
+			'tone-destructive',
+			'size-normal'
+		)
+		expect(button).not.toHaveClass('tone-normal')
+	})
+
+	it('applies different class names based on size', () => {
+		const { rerender } = render(
+			<Button variant='primary' size='normal'>
+				Click me
+			</Button>
+		)
+		const button = screen.getByRole('button', { name: 'Click me' })
+		expect(button).toHaveClass(
+			'variant-primary',
+			'tone-normal',
+			'size-normal'
+		)
+		expect(button).not.toHaveClass('size-small')
+		expect(button).not.toHaveClass('size-large')
+
+		rerender(
+			<Button variant='secondary' size='small'>
+				Click me
+			</Button>
+		)
+		expect(button).toHaveClass(
+			'variant-secondary',
+			'tone-normal',
+			'size-small'
+		)
+		expect(button).not.toHaveClass('size-normal')
+		expect(button).not.toHaveClass('size-large')
+
+		rerender(
+			<Button variant='tertiary' size='large'>
+				Click me
+			</Button>
+		)
+		expect(button).toHaveClass(
+			'variant-tertiary',
+			'tone-normal',
+			'size-large'
+		)
+		expect(button).not.toHaveClass('size-normal')
+		expect(button).not.toHaveClass('size-small')
+	})
+
+	it('should call onClick handler when clicked', () => {
+		const onClick = jest.fn()
+		render(
+			<Button variant='primary' onClick={onClick}>
+				Click me
+			</Button>
+		)
+		const button = screen.getByRole('button', { name: 'Click me' })
+		expect(button).not.toHaveAttribute('aria-disabled', 'true')
+		expect(onClick).not.toHaveBeenCalled()
+		fireEvent.click(button)
+		expect(onClick).toHaveBeenCalledTimes(1)
+	})
+
+	it('should ignore clicks when disabled', () => {
+		const onClick = jest.fn()
+		render(
+			<Button variant='primary' onClick={onClick} disabled>
+				Click me
+			</Button>
+		)
+		const button = screen.getByRole('button', { name: 'Click me' })
+		expect(button).toHaveAttribute('aria-disabled', 'true')
+		expect(onClick).not.toHaveBeenCalled()
+		fireEvent.click(button)
+		expect(onClick).not.toHaveBeenCalled()
+	})
+	it('submits a form when used with type="submit"', () => {
+		const onSubmit = jest.fn(event => event.preventDefault())
+
+		render(
+			<form onSubmit={onSubmit}>
+				<Button variant='primary' type='submit'>
+					Submit
+				</Button>
+			</form>
+		)
+		const button = screen.getByRole('button', { name: 'Submit' })
+		expect(button).not.toHaveAttribute('aria-disabled', 'true')
+		expect(button).toHaveAttribute('type', 'submit')
+		expect(onSubmit).not.toHaveBeenCalled()
+		fireEvent.click(button)
+		expect(onSubmit).toHaveBeenCalledTimes(1)
+	})
+
+	it('does not submits a form when disabled', () => {
+		const onSubmit = jest.fn(event => event.preventDefault())
+
+		render(
+			<form onSubmit={onSubmit}>
+				<Button variant='primary' type='submit' disabled>
+					Submit
+				</Button>
+			</form>
+		)
+		const button = screen.getByRole('button', { name: 'Submit' })
+		expect(button).toHaveAttribute('aria-disabled', 'true')
+		expect(onSubmit).not.toHaveBeenCalled()
+		fireEvent.click(button)
+		expect(onSubmit).not.toHaveBeenCalled()
+	})
+	it('renders its children as its content', () => {
+		render(
+			<Button variant='primary'>
+				Click me <strong>now</strong>
+			</Button>
+		)
+		expect(
+			screen.getByRole('button', { name: 'Click me now' }).innerHTML
+		).toMatchInlineSnapshot(
+			`"<span class=\\"label\\">Click me <strong>now</strong></span>"`
+		)
 	})
 })
--- libs/core-ui/src/lib/button/button.tsx
+++ libs/core-ui/src/lib/button/button.tsx
@@ -1,19 +1,67 @@
 import { ReactNode } from 'react'
 
 import { Box } from '../box'
+import { polymorphicComponent } from '../polymorphic'
 import styles from './button.module.less'
 
-/* eslint-disable-next-line */
-export interface ButtonProps {
-	children: ReactNode
+const preventDefault = (event: React.SyntheticEvent) => {
+	event.preventDefault()
 }
 
-export function Button({ children }: ButtonProps) {
-	return (
-		<Box as='button' className={styles['Button']}>
-			{children}
-		</Box>
-	)
+type ButtonVariant = 'primary' | 'secondary' | 'tertiary' | 'quaternary'
+type ButtonTone = 'normal' | 'destructive'
+type ButtonSize = 'small' | 'normal' | 'large'
+
+type CommonButtonProps = {
+	variant: ButtonVariant
+	tone?: ButtonTone
+	size?: ButtonSize
+	disabled?: boolean
+	loading?: boolean
 }
+
+/* eslint-disable-next-line */
+export type ButtonProps = {
+	children: NonNullable<ReactNode>
+} & CommonButtonProps
+
+const Button = polymorphicComponent<'button', ButtonProps>(
+	(
+		{
+			children,
+			variant = 'primary',
+			tone = 'normal',
+			size = 'normal',
+			disabled = false,
+			loading = false,
+			onClick,
+			...props
+		},
+		ref
+	) => {
+		const isDisabled = loading || disabled
+
+		return (
+			<Box
+				{...props}
+				as='button'
+				aria-disabled={isDisabled}
+				onClick={isDisabled ? preventDefault : onClick}
+				className={[
+					styles['baseButton'],
+					styles[`variant-${variant}`],
+					styles[`tone-${tone}`],
+					styles[`size-${size}`],
+					disabled ? styles['disabled'] : null,
+				]}
+				ref={ref}
+			>
+				{children ? (
+					<span className={styles['label']}>{children}</span>
+				) : null}
+			</Box>
+		)
+	}
+)
 
 export default Button
