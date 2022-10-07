ec54e0a95a102593bb3770fa8c25d8ff82ef1267
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-10-07 17:34:17 +0700

    feat(core-ui): add button blueprint

---
+++ libs/core-ui/src/lib/button/button.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from 'react'
+import { Box } from '../box'
+import styles from './button.module.less'
+
+/* eslint-disable-next-line */
+export interface ButtonProps {
+	children: ReactNode
+}
+
+export function Button({ children }: ButtonProps) {
+	return (
+		<Box as='button' className={styles['Button']}>
+			{children}
+		</Box>
+	)
+}
+
+export default Button
---
+++ libs/core-ui/src/lib/button/polybox.tsx
@@ -0,0 +1,20 @@
+import { forwardRef } from 'react'
+
+export type BoxOwnProps<E extends React.ElementType = React.ElementType> = {
+	as?: E
+}
+
+export type BoxProps<E extends React.ElementType> = BoxOwnProps<E> &
+	Omit<React.ComponentProps<E>, keyof BoxOwnProps>
+
+const defaultElement = 'div'
+
+export const Box: <E extends React.ElementType = typeof defaultElement>(
+	props: BoxProps<E>
+) => React.ReactElement | null = forwardRef(function Box(
+	props: BoxOwnProps,
+	ref: React.Ref<Element>
+) {
+	const Element = props.as || defaultElement
+	return <Element ref={ref} {...props} as={undefined} />
+})
