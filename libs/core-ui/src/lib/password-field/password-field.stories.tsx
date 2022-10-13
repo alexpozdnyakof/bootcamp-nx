e07ab6146bc7723d300ecca6c1e648a6c40311e3
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-10-14 03:26:25 +0700

    docs(password-field): add storybook

---
+++ libs/core-ui/src/lib/password-field/password-field.stories.tsx
@@ -0,0 +1,142 @@
+import { SyntheticEvent } from 'react'
+import { BoxMaxWidth } from '../box'
+import { Stack } from '../stack'
+import { Text } from '../text'
+import { PartialProps, selectWithNone } from '../storybook-helpers'
+import PasswordField from './password-field'
+
+export default {
+	title: 'Design system/PasswordField',
+	component: PasswordField,
+	parameters: {
+		badges: ['accessible'],
+	},
+}
+
+const preventDefault = (event: SyntheticEvent) => {
+	event.preventDefault()
+}
+
+export function InteractivePropsStory({
+	label,
+	auxiliaryLabel,
+	...props
+}: PartialProps<typeof PasswordField>) {
+	return (
+		<PasswordField
+			{...props}
+			label={label}
+			auxiliaryLabel={
+				auxiliaryLabel ? (
+					// eslint-disable-next-line jsx-a11y/anchor-is-valid
+					<a href='#' onClick={preventDefault}>
+						{auxiliaryLabel}
+					</a>
+				) : undefined
+			}
+		/>
+	)
+}
+
+InteractivePropsStory.argTypes = {
+	label: {
+		control: { type: 'text' },
+		defaultValue: 'Password',
+	},
+	secondaryLabel: {
+		control: { type: 'text' },
+		defaultValue: 'optional',
+	},
+	auxiliaryLabel: {
+		control: { type: 'text' },
+		defaultValue: 'Forgot your password?',
+	},
+	hint: {
+		control: { type: 'text' },
+		defaultValue:
+			'Must be at least 100 characters long, and it should include each letter of the alphabet',
+	},
+	message: {
+		control: { type: 'text' },
+		defaultValue: '',
+	},
+	tone: {
+		options: ['neutral', 'success', 'error', 'loading'],
+		control: { type: 'inline-radio' },
+		defaultValue: 'neutral',
+	},
+	variant: {
+		options: ['default', 'bordered'],
+		control: { type: 'inline-radio' },
+		defaultValue: 'default',
+	},
+	placeholder: {
+		control: { type: 'text' },
+		defaultValue: 'Type your password',
+	},
+	maxWidth: selectWithNone<BoxMaxWidth>(
+		['xsmall', 'small', 'medium', 'large', 'xlarge'],
+		'small'
+	),
+}
+
+export function MessageToneStory() {
+	return (
+		<Stack space='xxlarge'>
+			<PasswordField
+				label='Password confirmation'
+				message='Comparing to original password…'
+				tone='loading'
+				disabled
+				maxWidth='small'
+			/>
+			<PasswordField
+				label='Password confirmation'
+				message='It does not match the original password'
+				tone='error'
+				maxWidth='small'
+			/>
+			<PasswordField
+				label='Password confirmation'
+				message='Matches original password!'
+				tone='success'
+				maxWidth='small'
+			/>
+			<PasswordField
+				label='Password confirmation'
+				message='Message with neutral tone (used as description, but still prefer the hint prop for that)'
+				hint='This is the primary description of the field, provided by the hint prop'
+				tone='neutral'
+				maxWidth='small'
+			/>
+		</Stack>
+	)
+}
+
+export function WithoutLabelStory() {
+	return (
+		<Stack space='xlarge' maxWidth='small'>
+			<Stack as='label' htmlFor='custom-textarea' space='small'>
+				<Text size='subtitle'>Custom label is up here</Text>
+				<Text size='caption' tone='secondary' aria-hidden>
+					<em>(click me to focus the textarea)</em>
+				</Text>
+			</Stack>
+			<PasswordField
+				label={null}
+				id='custom-textarea'
+				aria-describedby='custom-description'
+				placeholder='Password field without a built-in label'
+			/>
+			<Stack space='small' id='custom-description'>
+				<Text size='body'>Custom description is down here</Text>
+				<Text size='caption' tone='secondary' aria-hidden>
+					<em>
+						(inspect the input element accessibility properties if
+						you are curious)
+					</em>
+				</Text>
+			</Stack>
+		</Stack>
+	)
+}
