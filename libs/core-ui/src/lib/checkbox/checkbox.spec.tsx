d7755f40e0e98616adf53e15655718d729e42b4f
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-10-13 17:46:33 +0700

    test(checkbox): add test for behavior

--- libs/core-ui/src/lib/checkbox/checkbox.spec.tsx
+++ libs/core-ui/src/lib/checkbox/checkbox.spec.tsx
@@ -1,10 +1,119 @@
-import { render } from '@testing-library/react'
+import { screen, render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { useState } from 'react'
 
 import Checkbox from './checkbox'
 
 describe('Checkbox', () => {
-	it('should render successfully', () => {
-		const { baseElement } = render(<Checkbox />)
-		expect(baseElement).toBeTruthy()
+	it('support externally provided id attribute', () => {
+		render(<Checkbox data-testid='checkbox' id='custom' label='Accept' />)
+		const checkboxElement = screen.getByTestId('checkbox')
+
+		expect(checkboxElement.id).toBe('custom')
+	})
+
+	it('labelled by label string', () => {
+		render(<Checkbox data-testid='checkbox' label='Accept' />)
+		const checkboxElement = screen.getByTestId('checkbox')
+
+		expect(checkboxElement).toHaveAccessibleName('Accept')
+	})
+
+	it('labelled by label react node', () => {
+		render(
+			<Checkbox
+				data-testid='checkbox'
+				label={<span data-testid='label'>Accept</span>}
+			/>
+		)
+		const checkboxElement = screen.getByTestId('checkbox')
+		const labelElement = screen.getByTestId('label')
+
+		expect(checkboxElement).toHaveAccessibleName('Accept')
+		expect(labelElement).toBeInTheDocument()
+	})
+
+	it('hidden when hidden truthy', () => {
+		const { rerender } = render(
+			<Checkbox data-testid='checkbox' label='Accept' hidden />
+		)
+		const checkboxElement = screen.getByTestId('checkbox')
+
+		expect(checkboxElement).not.toBeVisible()
+		expect(
+			screen.queryByRole('checkbox', { name: 'Accept' })
+		).not.toBeInTheDocument()
+
+		rerender(<Checkbox data-testid='checkbox' label='Accept' />)
+		expect(checkboxElement).toBeVisible()
+		expect(
+			screen.queryByRole('checkbox', { name: 'Accept' })
+		).toBeInTheDocument()
+	})
+
+	it('toggle checked when user clicked on it', async () => {
+		render(<Checkbox label='Accept' />)
+		const checkboxElement = screen.getByRole('checkbox', { name: 'Accept' })
+
+		expect(checkboxElement).not.toBeChecked()
+
+		await userEvent.click(checkboxElement)
+		expect(checkboxElement).toBeChecked()
+
+		await userEvent.click(checkboxElement)
+		expect(checkboxElement).not.toBeChecked()
+	})
+
+	it('not toggle checked when disabled', async () => {
+		render(<Checkbox label='Accept' disabled />)
+		const checkboxElement = screen.getByRole('checkbox', { name: 'Accept' })
+
+		expect(checkboxElement).toBeDisabled()
+		expect(checkboxElement).not.toBeChecked()
+
+		await userEvent.click(checkboxElement)
+		expect(checkboxElement).not.toBeChecked()
+	})
+
+	it('can be checked by default when uncontrolled', async () => {
+		render(<Checkbox label='Accept' defaultChecked />)
+		const checkboxElement = screen.getByRole('checkbox', { name: 'Accept' })
+
+		expect(checkboxElement).toBeChecked()
+		await userEvent.click(checkboxElement)
+		expect(checkboxElement).not.toBeChecked()
+	})
+
+	it('can be a controlled', async () => {
+		const TestCase = () => {
+			const [checked, setChecked] = useState(false)
+			return (
+				<>
+					<Checkbox
+						label='Accept'
+						checked={checked}
+						onChange={event =>
+							setChecked(event.currentTarget.checked)
+						}
+					/>
+					<div data-testid='value'>{checked ? 'on' : 'off'}</div>
+				</>
+			)
+		}
+
+		render(<TestCase />)
+		const checkboxElement = screen.getByRole('checkbox', { name: 'Accept' })
+		const valueElement = screen.getByTestId('value')
+
+		expect(checkboxElement).not.toBeChecked()
+		expect(valueElement).toHaveTextContent('off')
+
+		await userEvent.click(checkboxElement)
+		expect(checkboxElement).toBeChecked()
+		expect(valueElement).toHaveTextContent('on')
+
+		await userEvent.click(checkboxElement)
+		expect(checkboxElement).not.toBeChecked()
+		expect(valueElement).toHaveTextContent('off')
 	})
 })
--- libs/core-ui/src/lib/checkbox/checkbox.tsx
+++ libs/core-ui/src/lib/checkbox/checkbox.tsx
@@ -3,6 +3,34 @@ import { Box } from '../box'
 import { Text } from '../text'
 import styles from './checkbox.module.less'
 
+type IconProps = React.SVGProps<SVGSVGElement> & {
+	checked?: boolean
+	disabled?: boolean
+}
+
+const CheckboxIcon = ({ checked, disabled, ...props }: IconProps) => {
+	const getPath = () => {
+		const svgPath = {
+			checked:
+				'M18 4a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h12zm-2.457 4.293l-5.293 5.293-1.793-1.793a1 1 0 1 0-1.414 1.414l2.5 2.5a1 1 0 0 0 1.414 0l6-6a1 1 0 1 0-1.414-1.414z',
+			unchecked:
+				'M18 4a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h12zm0 1H6a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1z',
+			filled: 'M6 4H18C19.1046 4 20 4.89543 20 6V18C20 19.1046 19.1046 20 18 20H6C4.89543 20 4 19.1046 4 18V6C4 4.89543 4.89543 4 6 4Z',
+		} as const
+		if (checked) return svgPath.checked
+		if (disabled) return svgPath.filled
+		return svgPath.unchecked
+	}
+
+	const path = getPath()
+
+	return (
+		<svg width='24px' height='24px' viewBox='0 0 24 24' {...props}>
+			<path fill='currentColor' fillRule='nonzero' d={path} />
+		</svg>
+	)
+}
+
 type CheckboxProps = Omit<
 	JSX.IntrinsicElements['input'],
 	'type' | 'className' | 'disabled'
@@ -33,6 +61,7 @@ const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
 					ref={ref}
 					type='checkbox'
 					checked={isChecked}
+					disabled={disabled}
 					onChange={event => {
 						onChange?.(event)
 						if (!event.defaultPrevented) {
@@ -45,6 +74,11 @@ const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
 					onKeyUp={event => {
 						props?.onKeyUp?.(event)
 					}}
+				/>
+				<CheckboxIcon
+					checked={isChecked}
+					disabled={disabled}
+					aria-hidden
 				/>
 				{label ? <Text>{label}</Text> : null}
 			</Box>
