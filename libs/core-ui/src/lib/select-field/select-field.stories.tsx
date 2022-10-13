75a2db7af1c683c7f8a071c4d33e3642b0537173
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-10-14 02:15:08 +0700

    docs(select-field): add story

---
+++ libs/core-ui/src/lib/select-field/select-field.stories.tsx
@@ -0,0 +1,168 @@
+import { SyntheticEvent } from 'react'
+import { BoxMaxWidth } from '../box'
+import { Stack } from '../stack'
+import { Text } from '../text'
+import { PartialProps, selectWithNone } from '../storybook-helpers'
+import SelectField from './select-field'
+
+export default {
+	title: 'Design system/SelectField',
+	component: SelectField,
+	parametrs: {
+		badges: ['accessible'],
+	},
+}
+
+const preventDefault = (event: SyntheticEvent) => {
+	event.preventDefault()
+}
+
+export function InteractivePropsStory({
+	label,
+	auxiliaryLabel,
+	...props
+}: PartialProps<typeof SelectField>) {
+	return (
+		<SelectField
+			{...props}
+			label={label}
+			auxiliaryLabel={
+				auxiliaryLabel ? (
+					// eslint-disable-next-line jsx-a11y/anchor-is-valid
+					<a href='#' onClick={preventDefault}>
+						{auxiliaryLabel}
+					</a>
+				) : undefined
+			}
+			defaultValue='-'
+		>
+			<option value='-' disabled>
+				Select theme
+			</option>
+			<optgroup label='Light themes'>
+				<option value='default'>Default theme</option>
+				<option value='bright'>Extra bright</option>
+			</optgroup>
+			<optgroup label='Dark themes'>
+				<option value='contrast'>High contrast</option>
+				<option value='dark'>Dark mode</option>
+			</optgroup>
+		</SelectField>
+	)
+}
+
+InteractivePropsStory.argTypes = {
+	label: {
+		control: { type: 'text' },
+		defaultValue: 'Theme',
+	},
+	secondaryLabel: {
+		control: { type: 'text' },
+		defaultValue: 'optional',
+	},
+	auxiliaryLabel: {
+		control: { type: 'text' },
+		defaultValue: 'Need help?',
+	},
+	hint: {
+		control: { type: 'text' },
+		defaultValue:
+			'The theme you select will be applied immediately. If you upgrade to premium you will have more themes to choose from.',
+	},
+	message: {
+		control: { type: 'text' },
+		defaultValue: '',
+	},
+	tone: {
+		options: ['neutral', 'success', 'error', 'loading'],
+		control: { type: 'inline-radio' },
+		defaultValue: 'neutral',
+	},
+	maxWidth: selectWithNone<BoxMaxWidth>(
+		['xsmall', 'small', 'medium', 'large', 'xlarge'],
+		'small'
+	),
+}
+
+export function MessageToneStory() {
+	return (
+		<Stack space='xxlarge'>
+			<SelectField
+				label='Country of residence'
+				message='Saving…'
+				tone='loading'
+				disabled
+				maxWidth='small'
+			>
+				<option value='none' disabled>
+					–
+				</option>
+			</SelectField>
+
+			<SelectField
+				label='Country of residence'
+				message='Something went wrong. Please, try again.'
+				tone='error'
+				maxWidth='small'
+			>
+				<option value='none' disabled>
+					–
+				</option>
+			</SelectField>
+
+			<SelectField
+				label='Country of residence'
+				message='Saved successfully!'
+				tone='success'
+				maxWidth='small'
+			>
+				<option value='none' disabled>
+					–
+				</option>
+			</SelectField>
+
+			<SelectField
+				label='Country of residence'
+				message='Message with neutral tone (used as description, but still prefer the hint prop for that)'
+				hint='This is the primary description of the field, provided by the hint prop'
+				tone='neutral'
+				maxWidth='small'
+			>
+				<option value='none' disabled>
+					–
+				</option>
+			</SelectField>
+		</Stack>
+	)
+}
+
+export function WithoutLabelStory() {
+	return (
+		<Stack space='xlarge' maxWidth='small'>
+			<Stack as='label' htmlFor='custom-textarea' space='small'>
+				<Text size='subtitle'>Custom label is up here</Text>
+				<Text size='caption' tone='secondary' aria-hidden>
+					<em>(click me to focus the select element)</em>
+				</Text>
+			</Stack>
+			<SelectField
+				label={null}
+				id='custom-textarea'
+				aria-describedby='custom-description'
+			>
+				<option value='none' disabled>
+					–
+				</option>
+			</SelectField>
+			<Stack space='small' id='custom-description'>
+				<Text size='body'>Custom description is down here</Text>
+				<Text size='caption' tone='secondary' aria-hidden>
+					<em>
+						(inspect the select element accessibility properties if
+						you are curious)
+					</em>
+				</Text>
+			</Stack>
+		</Stack>
+	)
+}
