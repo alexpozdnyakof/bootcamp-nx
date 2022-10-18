b317699a9aac73c45530a334ca0614cd0dd44b3a
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-10-18 21:51:08 +0700

    refactor(checkbox): update checkbox styles with new design

--- libs/core-ui/src/lib/checkbox/checkbox.tsx
+++ libs/core-ui/src/lib/checkbox/checkbox.tsx
@@ -3,30 +3,16 @@ import { Box } from '../box'
 import { Text } from '../text'
 import styles from './checkbox.module.less'
 
-type IconProps = React.SVGProps<SVGSVGElement> & {
-	checked?: boolean
-	disabled?: boolean
-}
-
-const CheckboxIcon = ({ checked, disabled, ...props }: IconProps) => {
-	const getPath = () => {
-		const svgPath = {
-			checked:
-				'M18 4a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h12zm-2.457 4.293l-5.293 5.293-1.793-1.793a1 1 0 1 0-1.414 1.414l2.5 2.5a1 1 0 0 0 1.414 0l6-6a1 1 0 1 0-1.414-1.414z',
-			unchecked:
-				'M18 4a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h12zm0 1H6a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1z',
-			filled: 'M6 4H18C19.1046 4 20 4.89543 20 6V18C20 19.1046 19.1046 20 18 20H6C4.89543 20 4 19.1046 4 18V6C4 4.89543 4.89543 4 6 4Z',
-		} as const
-		if (checked) return svgPath.checked
-		if (disabled) return svgPath.filled
-		return svgPath.unchecked
-	}
-
-	const path = getPath()
-
+function CheckboxIcon(props: JSX.IntrinsicElements['svg']) {
 	return (
-		<svg width='24px' height='24px' viewBox='0 0 24 24' {...props}>
-			<path fill='currentColor' fillRule='nonzero' d={path} />
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			className={styles['checkbox_icon']}
+			{...props}
+		>
+			<g fill='#fff' fillRule='evenodd'>
+				<path d='M10.346 3.301a.929.929 0 0 1 1.37 0 1.076 1.076 0 0 1 0 1.456l-4.64 4.94a.929.929 0 0 1-1.37 0L3.284 7.123a1.076 1.076 0 0 1 0-1.456.929.929 0 0 1 1.37 0L6.39 7.513l3.955-4.212z' />
+			</g>
 		</svg>
 	)
 }
@@ -77,11 +63,15 @@ const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
 						props?.onKeyUp?.(event)
 					}}
 				/>
-				<CheckboxIcon
-					checked={isChecked}
-					disabled={disabled}
+				<Box
+					className={[
+						styles['checkbox'],
+						isChecked ? styles['checkbox__checked'] : null,
+					]}
 					aria-hidden
-				/>
+				>
+					<CheckboxIcon />
+				</Box>
 				{label ? <Text>{label}</Text> : null}
 			</Box>
 		)
