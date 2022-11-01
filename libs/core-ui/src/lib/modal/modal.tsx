59576785a7942189bb62e6d59f47eae25f992920
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-11-01 20:22:21 +0700

    extract overlay, backdrop, center and portal

--- libs/core-ui/src/lib/modal/modal.tsx
+++ libs/core-ui/src/lib/modal/modal.tsx
@@ -1,4 +1,19 @@
+import { ReactNode } from 'react'
+import { createPortal } from 'react-dom'
+import { Box } from '../box'
 import styles from './modal.module.less'
+
+function Portal({ children }: { children: ReactNode }) {
+	return createPortal(children, document.body)
+}
+
+const Backdrop = ({ children }: { children: ReactNode }) => {
+	return <Box className={styles['backdrop']}>{children}</Box>
+}
+
+const Center = ({ children }: { children: ReactNode }) => {
+	return <Box className={styles['center']}>{children}</Box>
+}
 
 type ModalProps = {
 	isOpen: boolean
@@ -7,9 +22,19 @@ type ModalProps = {
 export function Modal({ isOpen, ...props }: ModalProps) {
 	if (!isOpen) return null
 	return (
-		<div className={styles['container']} {...props}>
-			<h1>Welcome to Modal!</h1>
-		</div>
+		<Portal>
+			<Backdrop>
+				<Center>
+					<Box className={styles['dialog']} {...props}>
+						<Box className={styles['dialog-content']} {...props}>
+							<Box className={styles['center']}>
+								<h1>Welcome to Modal!</h1>
+							</Box>
+						</Box>
+					</Box>
+				</Center>
+			</Backdrop>
+		</Portal>
 	)
 }
 
