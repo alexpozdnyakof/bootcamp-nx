072e1ba69433c8e18cbc650ac426027490a8e751
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-11-03 16:28:22 +0700

    feat(tooltip): add portal rendering  and mouse reaction

--- libs/core-ui/src/lib/tooltip/tooltip.spec.tsx
+++ libs/core-ui/src/lib/tooltip/tooltip.spec.tsx
@@ -1,10 +1,50 @@
-import { render } from '@testing-library/react'
+import { screen, render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { Button } from '../button'
 
 import Tooltip from './tooltip'
 
 describe('Tooltip', () => {
 	it('should render successfully', () => {
-		const { baseElement } = render(<Tooltip />)
+		const { baseElement } = render(
+			<Tooltip>
+				<Button>私を指して</Button>
+			</Tooltip>
+		)
 		expect(baseElement).toBeTruthy()
+	})
+
+	xit('should show tooltip', async () => {
+		render(
+			<Tooltip data-testid='tooltip'>
+				<Button>私を指して</Button>
+			</Tooltip>
+		)
+
+		const tooltip = screen.getByTestId('tooltip')
+		expect(tooltip).not.toBeInTheDocument()
+
+		const buttonElement = screen.getByRole('button', { name: '私を指して' })
+
+		await userEvent.hover(buttonElement)
+		expect(tooltip).toBeInTheDocument()
+
+		await userEvent.unhover(buttonElement)
+		expect(tooltip).not.toBeInTheDocument()
+	})
+
+	it('should render tooltip in portal', async () => {
+		const { baseElement } = render(
+			<Tooltip data-testid='tooltip'>
+				<Button>私を指して</Button>
+			</Tooltip>
+		)
+
+		const portal = baseElement.querySelector('#__portalRoot__')
+
+		const buttonElement = screen.getByRole('button', { name: '私を指して' })
+
+		await userEvent.hover(buttonElement)
+		expect(portal?.textContent?.trim()).toBe('Welcome to Tooltip!')
 	})
 })
--- libs/core-ui/src/lib/tooltip/tooltip.tsx
+++ libs/core-ui/src/lib/tooltip/tooltip.tsx
@@ -1,14 +1,52 @@
+import { cloneElement, MouseEvent, ReactElement, useState } from 'react'
+import { Box } from '../box'
+import { polymorphicComponent } from '../polymorphic'
+import { Portal } from '../portal'
 import styles from './tooltip.module.less'
 
-/* eslint-disable-next-line */
-export interface TooltipProps {}
-
-export function Tooltip(props: TooltipProps) {
-	return (
-		<div className={styles['container']}>
-			<h1>Welcome to Tooltip!</h1>
-		</div>
-	)
+type TooltipProps = {
+	children: ReactElement
 }
 
+/**
+ *
+ * TODO: wrap children
+ * TODO: set listeners for mouse
+ * TODO: show or hide tooltip
+ * @param props
+ * @returns
+ */
+
+const Tooltip = polymorphicComponent<'div', TooltipProps>(
+	({ children, ...props }) => {
+		const [visible, setVisible] = useState<boolean>(false)
+		if (!children) return null
+		const onMouseOver = (e: MouseEvent) => {
+			setVisible(true)
+		}
+		const onMouseOut = (e: MouseEvent) => {
+			setVisible(false)
+		}
+
+		const withMouseListeners = cloneElement(children, {
+			onMouseOver,
+			onMouseOut,
+		})
+
+		return (
+			<>
+				{withMouseListeners}
+				<Portal>
+					{visible && (
+						<Box className={styles['container']} {...props}>
+							Welcome to Tooltip!
+						</Box>
+					)}
+				</Portal>
+			</>
+		)
+	}
+)
+
+Tooltip.displayName = 'Tooltip'
 export default Tooltip
