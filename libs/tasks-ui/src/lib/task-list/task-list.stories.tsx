735fb799f94c4999f7f069a4e3a73df458470308
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-11-11 16:33:33 +0700

    docs(task-list): remove state from storybook

--- libs/tasks-ui/src/lib/task-list/task-list.stories.tsx
+++ libs/tasks-ui/src/lib/task-list/task-list.stories.tsx
@@ -1,60 +1,13 @@
 import { Box } from '@bootcamp-nx/core-ui'
-import { ComponentProps, useCallback, useMemo, useState } from 'react'
-import { ViewTask } from '../common-types'
-import { TaskList } from './task-list'
 import { TaskListProvider } from '../task-list-context'
-
-const tasks = [
-	{
-		id: 0,
-		text: '血液レポートのグラフが空白になっている',
-		done: false,
-	},
-	{
-		id: 1,
-		text: '無効にする|| ユーザーがアカウントを無効にできない',
-		done: true,
-	},
-	{
-		id: 2,
-		text: '|| 私のプロフィール || ユーザーは、サインアップ時に設定された体重と身長を表示できません',
-		done: false,
-	},
-	{
-		id: 3,
-		text: 'プロフィール、プロフィールの編集、ポップアップ',
-		done: true,
-	},
-	{
-		id: 4,
-		text: 'ビルドを共有するには Apple 開発者アカウントが必要です',
-		done: false,
-	},
-]
+import { TaskList } from './task-list'
 
 export default {
 	component: TaskList,
 	title: 'Tasks/TaskList',
 }
 
-type TaskListState = {
-	tasks: Array<ViewTask>
-	active: ViewTask['id'] | null
-}
-
-/**
- * TODO: Lifting state
- * 1. create task from form
- * 2. delete task
- * 3. start task editing
- * 4. end task editing
- * 5. change task text
- * 6. change task status
- * @param param0
- * @returns
- */
-
-export function Interactive({ tasks, title }: ComponentProps<typeof TaskList>) {
+export function Interactive() {
 	return (
 		<Box marginLeft='xlarge'>
 			<TaskListProvider>
@@ -62,9 +15,4 @@ export function Interactive({ tasks, title }: ComponentProps<typeof TaskList>) {
 			</TaskListProvider>
 		</Box>
 	)
-}
-
-Interactive.args = {
-	tasks,
-	title: '素晴らしいタスクリスト',
 }
