130554d46921199c2986f5e444fbb503b030afa2
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-11-22 21:52:46 +0700

    refactor: move projects to own directory

--- apps/api/src/app/projects/project-model.ts
+++ apps/api/src/app/projects/project-model.ts
@@ -1,5 +1,5 @@
-import DataModel from './data-model'
-import { UniqueId, UpdatedCreatedTime } from './data-unit'
+import DataModel from '../data-model'
+import { UniqueId, UpdatedCreatedTime } from '../data-unit'
 export type Value = {
 	id: number
 	title: string
