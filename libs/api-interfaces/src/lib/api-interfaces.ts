fe96178de320088d9003d9eb59c02dd1a7e876bf
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-12-01 16:58:02 +0700

    feat(api-interfaces): add project

--- libs/api-interfaces/src/lib/api-interfaces.ts
+++ libs/api-interfaces/src/lib/api-interfaces.ts
@@ -1,3 +1,12 @@
 export interface Message {
   message: string;
 }
+export interface ApiProject {
+	id: number
+	title: string
+	description: string | null
+	created: string
+	updated: string
+	tasklists: Array<number>
+	type: 'project'
+}
