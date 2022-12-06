f543d728365c7f37ff8ff83e62072e6658d00d30
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-12-06 17:14:58 +0700

    feat(api): add app router

--- apps/api/src/app/app.ts
+++ apps/api/src/app/app.ts
@@ -1,25 +1,13 @@
-import { Message } from '@bootcamp-nx/api-interfaces'
 import express from 'express'
 import morgan from 'morgan'
-import { ProjectController, ProjectRouterPrefix } from './projects'
-import { TasklistController, TasklistRouterPrefix } from './tasklist'
-import { TaskController, TaskRouterPrefix } from './task'
+import AppRouter from './app-router'
 
 const app = express()
-const router = express.Router()
 
 morgan.token('body', req => JSON.stringify(req.body))
+
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 
-const greeting: Message = { message: 'Welcome to api!' }
-
-router.get('/api', (req, res) => {
-	res.send(greeting)
-})
-
-app.use(`/${ProjectRouterPrefix}`, ProjectController)
-app.use(`/${TasklistRouterPrefix}`, TasklistController)
-app.use(`/${TaskRouterPrefix}`, TaskController)
-app.use('/', router)
+app.use('/api', AppRouter)
 
 export default app
