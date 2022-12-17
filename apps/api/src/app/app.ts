9b22452d356296e9dd8c3b4c10d34d6a827260c3
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-12-17 22:59:25 +0700

    feat(api): add body parsing middleware

--- apps/api/src/app/app.ts
+++ apps/api/src/app/app.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
+import { json } from 'body-parser'
 import AppRouter from './app-router'
 import { database } from './database'
 
@@ -8,8 +10,10 @@ const app = express()
 database.migrate.latest().then(() => database.seed.run())
 morgan.token('body', req => JSON.stringify(req.body))
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+app.use(cors())
+app.use(json())
 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 app.use('/api', AppRouter)
 
 export default app
