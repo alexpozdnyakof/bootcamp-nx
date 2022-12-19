82047a2d3ed9a5b40c104169b506daf5b8a86cec
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-12-19 19:25:15 +0700

    refactor(api/tasklist): add typings for requests and responses

--- apps/api/src/app/tasklist/tasklist-controller.ts
+++ apps/api/src/app/tasklist/tasklist-controller.ts
@@ -1,5 +1,9 @@
-import { ApiTask, ApiTaskList } from '@bootcamp-nx/api-interfaces'
-import { Router, Response } from 'express'
+import {
+	ApiTask,
+	ApiTaskList,
+	ApiTaskListDTO,
+} from '@bootcamp-nx/api-interfaces'
+import { Router } from 'express'
 import { CreateTask } from '../task/task'
 import { TypedRequest } from '../typed-request'
 import { TypedResponse } from '../typed-response'
@@ -12,19 +16,25 @@ const TasklistRouterPrefix = 'tasklist'
 
 TasklistRouter.get(
 	'/:id',
-	async (request, response: Response<ApiTaskList | { message: string }>) => {
+	async (
+		request: TypedRequest<{ params: { id: string } }>,
+		response: TypedResponse<ApiTaskList>
+	) => {
 		const id = Number(request.params.id)
 		try {
 			const tasklist = await TaskListModel.GetOne(id)
 			response.status(200).send(CreateTaskList(tasklist))
 		} catch (error) {
-			response.status(404).send({ message: error.message })
+			response.status(404).send({ code: 404, message: error.message })
 		}
 	}
 )
 TasklistRouter.get(
 	'/:id/tasks',
-	async (request, response: Response<ApiTask[] | { message: string }>) => {
+	async (
+		request: TypedRequest<{ params: { id: string } }>,
+		response: TypedResponse<ApiTask[]>
+	) => {
 		const id = Number(request.params.id)
 		try {
 			const tasks = (await TaskListModel.GetRelatedTasks(id)).map(task =>
@@ -32,7 +42,7 @@ TasklistRouter.get(
 			)
 			response.status(200).send(tasks)
 		} catch (error) {
-			response.status(404).send({ message: error.message })
+			response.status(404).send({ code: 404, message: error.message })
 		}
 	}
 )
@@ -55,37 +65,55 @@ TasklistRouter.post(
 	}
 )
 
-TasklistRouter.post('/', async (req, res) => {
-	try {
-		const dto = TaskListValue.check(req.body)
-		const result = await TaskListModel.Add(dto)
-		res.status(201).send(result)
-	} catch (error) {
-		res.status(400).send({ message: error.message })
+TasklistRouter.post(
+	'/',
+	async (
+		req: TypedRequest<{ body: ApiTaskListDTO }>,
+		res: TypedResponse<{ id: number }>
+	) => {
+		try {
+			const dto = TaskListValue.check(req.body)
+			const result = await TaskListModel.Add(dto)
+			res.status(201).send(result)
+		} catch (error) {
+			res.status(400).send({ code: 400, message: error.message })
+		}
 	}
-})
+)
 
-TasklistRouter.delete('/:id', async (req, res) => {
-	const id = Number(req.params.id)
-	try {
-		const tasklist = await TaskListModel.GetOne(id)
-		if (typeof tasklist == 'undefined') throw new Error('Not Found')
-		await TaskListModel.Delete(id)
-		res.status(204).send()
-	} catch (error) {
-		res.status(400).send({ message: error.message })
+TasklistRouter.delete(
+	'/:id',
+	async (
+		req: TypedRequest<{ params: { id: string } }>,
+		res: TypedResponse<void>
+	) => {
+		const id = Number(req.params.id)
+		try {
+			const tasklist = await TaskListModel.GetOne(id)
+			if (typeof tasklist == 'undefined') throw new Error('Not Found')
+			await TaskListModel.Delete(id)
+			res.sendStatus(204)
+		} catch (error) {
+			res.status(400).send({ code: 400, message: error.message })
+		}
 	}
-})
+)
 
-TasklistRouter.put('/:id', async (req, res) => {
-	const id = Number(req.params.id)
-	try {
-		const dto = TaskListValue.check(req.body)
-		await TaskListModel.Update(id, dto)
-		res.status(204).send()
-	} catch (error) {
-		res.status(400).send({ message: error.message })
+TasklistRouter.put(
+	'/:id',
+	async (
+		req: TypedRequest<{ params: { id: string }; body: ApiTaskListDTO }>,
+		res: TypedResponse<void>
+	) => {
+		const id = Number(req.params.id)
+		try {
+			const dto = TaskListValue.check(req.body)
+			await TaskListModel.Update(id, dto)
+			res.status(204).send()
+		} catch (error) {
+			res.status(400).send({ code: 400, message: error.message })
+		}
 	}
-})
+)
 
 export { TasklistRouter as TasklistController, TasklistRouterPrefix }
